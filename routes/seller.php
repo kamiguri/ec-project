365e047e4b63b08755fe00c2f2f69db812844dd4
kamiguri <d.kamiguri@core-tech.jp>
2024-06-20 19:38:28 +0900

    sellerのルートをdashboardのみに修正

--- routes/seller.php
+++ routes/seller.php
@@ -8,10 +8,5 @@ Route::get('/dashboard', function () {
     return view('seller.dashboard');
 })->middleware(['auth:sellers', 'verified'])->name('dashboard');
 
-Route::middleware('auth:sellers')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/sellerAuth.php';
