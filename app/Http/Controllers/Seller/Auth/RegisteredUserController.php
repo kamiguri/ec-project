5ed482a95910662046c3cd6bed5cd337ed51c130
kamiguri <d.kamiguri@core-tech.jp>
2024-06-20 18:40:19 +0900

    Seller新規登録用コントローラのview名、リダイレクト先、modelクラスを修正

--- app/Http/Controllers/Seller/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Seller/Auth/RegisteredUserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Seller\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\Selelr;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -19,7 +19,7 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        return view('seller.auth.register');
     }
 
     /**
@@ -31,11 +31,11 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.Seller::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
-        $user = User::create([
+        $user = Seller::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
@@ -45,6 +45,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('seller.dashboard', absolute: false));
     }
 }
