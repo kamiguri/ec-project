e3d2a3a931c3b96c655f6aa51bcca579ca509b51
kamiguri <d.kamiguri@core-tech.jp>
2024-06-21 10:47:40 +0900

    Sellerのファクトリを追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Seller;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,6 +19,11 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        Seller::factory()->create([
+            'name' => 'Test Seller',
+            'email' => 'seller@example.com',
         ]);
 
         $this->call([
