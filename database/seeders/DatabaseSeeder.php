6da26ae699ebfce4a3cffc45d0d78a58a655e443
kamiguri <d.kamiguri@core-tech.jp>
2024-06-21 10:57:19 +0900

    Itemのカテゴリ

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Item;
 use App\Models\User;
 use App\Models\Seller;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -29,5 +30,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
         ]);
+
+        Item::factory()->create();
     }
 }
