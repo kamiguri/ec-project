ae7c1b0e73ac56660537fa705dd076781d086260
kamiguri <d.kamiguri@core-tech.jp>
2024-06-24 16:04:35 +0900

    購入画面を追加

---
+++ resources/views/user/purchase/create.blade.php
@@ -0,0 +1,25 @@
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <x-navigation-link title="タイトル" :skillId="1"/>
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h1>注文の確認</h1>
+                    @foreach (Auth::user()->cartItems as $item)
+                    <div>
+                        商品名: {{ $item->name }}
+                        金額: {{ $item->price }}
+                        数量: {{ $item->pivot->amount }}
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
