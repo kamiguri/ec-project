cf1fffa7e6ac327ee796458e0faa1e2f1f63a6b7
kamiguri <d.kamiguri@core-tech.jp>
2024-06-24 11:53:36 +0900

    カートから削除ボタンを追加

--- resources/views/user/cart/index.blade.php
+++ resources/views/user/cart/index.blade.php
@@ -4,5 +4,10 @@
 <div>
     商品名: {{ $item->name }}
     数量: {{ $item->pivot->amount }}
+    <form action="{{ route('cart.destroy', $item->id) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit">削除</button>
+    </form>
 </div>
 @endforeach
