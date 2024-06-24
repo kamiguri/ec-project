fea5ca9c6f5165c625cc83ddb0af1082264cb675
kamiguri <d.kamiguri@core-tech.jp>
2024-06-24 13:48:02 +0900

    カートの商品数量変更フォームを追加

--- resources/views/user/cart/index.blade.php
+++ resources/views/user/cart/index.blade.php
@@ -3,7 +3,14 @@
 @foreach (Auth::user()->cartItems as $item)
 <div>
     商品名: {{ $item->name }}
-    数量: {{ $item->pivot->amount }}
+    <form action="{{ route('cart.update', $item->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <label>数量:
+            <input type="number" name="amount" value="{{ $item->pivot->amount }}">
+        </label>
+        <button type="submit">更新</button>
+    </form>
     <form action="{{ route('cart.destroy', $item->id) }}" method="POST">
         @csrf
         @method('DELETE')
