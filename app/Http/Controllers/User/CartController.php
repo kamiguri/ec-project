48410deb1ba38ea0a7320ef934b9843742af46c6
kamiguri <d.kamiguri@core-tech.jp>
2024-06-24 10:07:14 +0900

    cartのaddコントローラをstoreに変更 カートに追加処理を追加

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Item;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
@@ -14,8 +15,10 @@ class CartController extends Controller
         return view('user.cart.index');
     }
 
-    public function add(Request $request, Item $item)
+    public function store(Request $request, $item_id)
     {
+        $item = Item::find($item_id);
+
         // 1. 在庫チェック
         if ($item->stock === 0) {
             return redirect()->back()->with('error', '在庫切れです');
@@ -23,6 +26,8 @@ class CartController extends Controller
 
         // 2. カートへの追加処理 (セッション or DB)
         $user = Auth::user();
+
+        $user->cartItems()->syncWithoutDetaching([$item_id => ['amount' => 1]]);
         // ... ここにカートへの追加ロジックを実装 ...
 
         return redirect()->route('items.show', $item)->with('success', 'カートに追加しました');
