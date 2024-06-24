7f6770a8edae2f158f0f40056537165ef49c789c
sunho-choe <s.choe@core-tech.jp>
2024-06-24 17:11:27 +0900

    登録者側のプロフィールを更新できた

--- resources/views/profile/seller/partials/update-password-form.blade.php
+++ resources/views/profile/seller/partials/update-password-form.blade.php
@@ -7,7 +7,7 @@
 
     </header>
 
-    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('seller.password.update') }}" class="mt-6 space-y-6">
         @csrf
         @method('put')
 
