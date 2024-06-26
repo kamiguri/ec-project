4014e3fb8d3c35d8581278b4386e6beb7d4db888
kamiguri <d.kamiguri@core-tech.jp>
2024-06-26 13:55:59 +0900

    月別売上のローカルスコープをItemモデルに追加

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -50,5 +51,17 @@ class Item extends Model
     public function usersWhoLiked()
     {
         return $this->belongsToMany(User::class, 'favorites', 'item_id', 'user_id');
+    }
+
+    public function scopeMonthlySales(Builder $query, $seller_id)
+    {
+        $query->selectRaw(
+                'SUM(order_items.price *  order_items.amount) AS sales, DATE_FORMAT(orders.created_at, "%Y-%m") AS month'
+            )
+            ->leftJoin('order_items', 'items.id', '=', 'order_items.item_id')
+            ->leftJoin('orders', 'order_items.order_id', '=', 'orders.id')
+            ->where('items.seller_id', $seller_id)
+            ->groupByRaw('DATE_FORMAT(orders.created_at, "%Y-%m")')
+            ->orderByRaw('DATE_FORMAT(orders.created_at, "%Y-%m") DESC');
     }
 }
