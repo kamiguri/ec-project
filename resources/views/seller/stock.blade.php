822d6d360872311841f2b345d765ad4fce64c19c
morita-coretech <k.morita@core-tech.jp>
2024-06-26 11:52:49 +0900

    layout

--- resources/views/seller/stock.blade.php
+++ resources/views/seller/stock.blade.php
@@ -1,28 +1,26 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('在庫登録画面') }}
+        </h2>
+    </x-slot>
 
-</body>
-</html>
-<h1>在庫登録画面</h1>
-
-<h2>商品名:{{$item->name}}</h2>
-<h2>在庫数:{{$item->stock}}</h2>
-
-<form action="/seller/item/{{$item->id}}/stock" method="post">
-@csrf
-<h3>在庫数:<input type="text" name="stock" value=""></h3>
-
-<div>
-    <button type="submit">更新する</button>
-</div>
-
-</form>
-
-<a href="{{ route('seller.items.index') }}">商品一覧へ</a>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <h2>商品名:{{$item->name}}</h2>
+                    <h2>在庫数:{{$item->stock}}</h2>
+                    <form action="/seller/item/{{$item->id}}/stock" method="post">
+                    @csrf
+                    <h3>在庫数:<input type="text" name="stock" value=""></h3>
+                    <div>
+                        <button type="submit">更新する</button>
+                    </div>
+                    </form>
+                    <a href="{{ route('seller.items.index') }}">商品一覧へ</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
