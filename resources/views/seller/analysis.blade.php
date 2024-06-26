fd98b7c4bb3b96cae723b5e12a55c2495bcab554
kamiguri <d.kamiguri@core-tech.jp>
2024-06-26 10:00:05 +0900

    チャートのサンプルを追加

--- resources/views/seller/analysis.blade.php
+++ resources/views/seller/analysis.blade.php
@@ -15,6 +15,9 @@
                                 <div class="card">
                                     <div class="card-header">{{ __('売上チャート') }}</div>
                                     <div class="card-body">
+                                        <div>
+                                            <canvas id="myChart"></canvas>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -24,4 +27,27 @@
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        const ctx = document.getElementById('myChart')
+
+        new Chart(ctx, {
+            type: 'bar',
+                data: {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1
+                }]
+                },
+                options: {
+                scales: {
+                    y: {
+                    beginAtZero: true
+                    }
+                }
+            }
+        })
+    </script>
 </x-app-layout>
