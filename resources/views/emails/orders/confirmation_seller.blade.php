e63467ed913d5ca9c2801d19e43cde3b5fecedaa
kamiguri <d.kamiguri@core-tech.jp>
2024-06-27 17:26:58 +0900

    注文の合計金額算出処理を修正

--- resources/views/emails/orders/confirmation_seller.blade.php
+++ resources/views/emails/orders/confirmation_seller.blade.php
@@ -11,7 +11,13 @@
         </tr>
     </thead>
     <tbody>
+        @php
+            $totalPrice = 0;
+        @endphp
         @foreach ($items as $item)
+            @php
+                $totalPrice += $item->pivot->amount * $item->pivot->price;
+            @endphp
             <tr>
                 <td>{{ $item->name }}</td>
                 <td>{{ $item->pivot->amount }}</td>
@@ -21,6 +27,6 @@
     </tbody>
 </table>
 
-<p>合計金額: {{ $items->sum('pivot.price') }}円</p>
+<p>合計金額: {{ $totalPrice }}円</p>
 
 <p>詳細は管理画面をご確認ください。</p>
