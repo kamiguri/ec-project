09c2aa914a7f1786d35f420b19d46803b4473538
kamiguri <d.kamiguri@core-tech.jp>
2024-06-28 09:35:40 +0900

    金額表示を変更

--- resources/views/user/cart/index.blade.php
+++ resources/views/user/cart/index.blade.php
@@ -1,21 +1,20 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            カートの商品一覧
+            カートの商品
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     @if ($cartItems->isNotEmpty())
+                        <div>合計 ¥{{ number_format($totalPrice) }}</div>
                         <div>
                             <a href="{{ route('purchase.create') }}">
                                 <button>レジに進む</button>
                             </a>
                         </div>
-                    @endif
                     @error('amount')
                         {{ $message }}
                     @enderror
@@ -25,7 +24,7 @@
                                 <li>商品名: {{ $item->name }}</li>
                                 <li><img src="{{ asset($item->photo_path) }}" alt="{{ $item->name }}" class="img-fluid"
                                         style="max-height: 200px;"></li>
-                                <li>価格: {{ $item->price }}円(JPY)</li>
+                                <li>価格: ¥{{ number_format($item->price) }}</li>
                                 <li>在庫数: {{ $item->stock }}</li>
                             </ul>
                             <form action="{{ route('cart.update', $item->id) }}" method="POST">
@@ -43,7 +42,10 @@
                             </form>
                         </div>
                     @endforeach
-                    <div>合計金額{{ $totalPrice }}円</div>
+                    @else
+                        <p>カートに商品はありません</p>
+                        <p><a href="{{ route('index') }}">商品を探す</a></p>
+                    @endif
                 </div>
             </div>
         </div>
