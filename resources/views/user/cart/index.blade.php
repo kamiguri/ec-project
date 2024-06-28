267d151d4939f3a48e541e21e81d5b22b3f1f648
kamiguri <d.kamiguri@core-tech.jp>
2024-06-28 11:19:34 +0900

    ボタン・レイアウトを変更

--- resources/views/user/cart/index.blade.php
+++ resources/views/user/cart/index.blade.php
@@ -1,57 +1,87 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            カートの商品
+            ショッピングカート
         </h2>
     </x-slot>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="flex items-start gap-8 max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if ($cartItems->isNotEmpty())
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    @if ($cartItems->isNotEmpty())
-                        <div>
+                        <div class="mb-5">
                             <p>合計 ¥
                                 <span class="text-lg font-semibold">{{ number_format($totalPrice) }}</span>
                             </p>
                         </div>
-                        <div>
+                        <div class="mb-5">
                             <a href="{{ route('purchase.create') }}">
-                                <button>レジに進む</button>
+                                <x-primary-button class="w-full" type="button">
+                                    {{ __('レジに進む') }}
+                                </x-primary-button>
                             </a>
                         </div>
                     @error('amount')
-                        {{ $message }}
+                        <p class="text-red-500">{{ $message }}</p>
                     @enderror
-                    @foreach ($cartItems as $item)
-                        <div>
-                            <ul>
-                                <li>商品名: {{ $item->name }}</li>
-                                <li><img src="{{ asset($item->photo_path) }}" alt="{{ $item->name }}" class="img-fluid"
-                                        style="max-height: 200px;"></li>
-                                <li>価格: ¥<span class="text-lg font-semibold">{{ number_format($item->price) }}</span></li>
-                                <li>在庫数: {{ $item->stock }}</li>
-                            </ul>
-                            <form action="{{ route('cart.update', $item->id) }}" method="POST">
-                                @csrf
-                                @method('PUT')
-                                <label>数量:
-                                    <input type="number" name="amount" value="{{ $item->pivot->amount }}">
-                                </label>
-                                <button type="submit">更新</button>
-                            </form>
-                            <form action="{{ route('cart.destroy', $item->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit">削除</button>
-                            </form>
-                        </div>
-                    @endforeach
-                    @else
-                        <p>カートに商品はありません</p>
-                        <p><a href="{{ route('index') }}">商品を探す</a></p>
-                    @endif
                 </div>
             </div>
+            <div class="grow bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 divide-y">
+                    <p class="text-xl">商品一覧</p>
+                    @foreach ($cartItems as $item)
+                        <div class="my-5 py-5">
+                            <a href="{{ route('show', $item->id) }}">
+                                <p class="text-lg">{{ $item->name }}</p>
+                                <div>
+                                    <img src="{{ asset($item->photo_path) }}" alt="{{ $item->name }}" class="img-fluid"
+                                        style="max-height: 200px;">
+                                </div>
+                            </a>
+                            <p>¥<span class="text-lg font-semibold">{{ number_format($item->price) }}</span></p>
+                            <div class="flex items-center">
+                                <form action="{{ route('cart.update', $item->id) }}" method="POST">
+                                    @csrf
+                                    @method('PUT')
+                                    <div class="flex items-center">
+                                        <x-input-label for="amount-input" :value="__('数量')" />
+
+                                        <x-text-input id="amount-input" class="block ms-3 mt-1"
+                                                        type="number"
+                                                        name="amount"
+                                                        value="{{ $item->pivot->amount }}"
+                                                        required />
+                                        <x-primary-button class="ms-3">
+                                            {{ __('更新') }}
+                                        </x-primary-button>
+                                    </div>
+                                </form>
+                                <form action="{{ route('cart.destroy', $item->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <x-secondary-button class="ms-3" type="submit">
+                                        {{ __('削除') }}
+                                    </x-secondary-button>
+                                </form>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            @else
+            <div class="grow bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <p>カートに商品はありません</p>
+                    <div class="my-3">
+                        <a href="{{ route('index') }}">
+                            <x-secondary-button>
+                                {{ __('商品を探す')}}
+                            </x-secondary-button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            @endif
         </div>
     </div>
 </x-app-layout>
